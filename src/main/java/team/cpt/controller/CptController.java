d97480b85b5580e79bbe8dc9d593a9877c076577
angerrookie <isarookie@163.com>
2019-08-19 15:41:43 +0800

    corporation

--- src/main/java/team/cpt/controller/CptController.java
+++ src/main/java/team/cpt/controller/CptController.java
@@ -1,16 +1,15 @@
 package team.cpt.controller;
 
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import team.cpt.pojo.Cpt;
 import team.cpt.pojo.Sort;
-import team.cpt.pojo.User;
 import team.cpt.service.ICptService;
 import team.cpt.service.IUserService;
 
@@ -24,7 +23,7 @@ import java.util.List;
  * 文件说明:
  */
 @Controller
-public class UserController {
+public class CptController {
 
     @Autowired
     @Qualifier("userService")
